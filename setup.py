afb8da8835921b8da4c934f721f03ea3f995841c
Schmidtke <f.schmidtke@iaew.rwth-aachen.de>
2023-12-01 23:09:47 +0100

    test distutils instead of setuptools

--- setup.py
+++ setup.py
@@ -34,8 +34,7 @@ import os
 import shutil
 
 from setuptools import setup
-from setuptools.command.develop import develop
-from setuptools.command.install import install
+from distutils.command.install import install
 
 
 def install_styles():
@@ -69,14 +68,6 @@ class PostInstallMoveFile(install):
         atexit.register(install_styles)
 
 
-class PostInstallDevMoveFile(develop):
-    """Post-installation in develop (editable) mode class to run the installation script"""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        atexit.register(install_styles)
-
-
 # Get description from README
 root = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(root, 'README.md'), encoding='utf-8') as f:
@@ -101,6 +92,5 @@ setup(
     ],
     url="https://github.com/skortmann/RWTHPlots",
     install_requires=['matplotlib', ],
-    cmdclass={'install': PostInstallMoveFile,
-              'develop': PostInstallDevMoveFile},
+    cmdclass={'install': PostInstallMoveFile, }
 )
